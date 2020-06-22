f54b6aa242bd01603c0f5b69b07393b4fcfd8374
Dorokhova Mari <test1910md@yandex.ru>
2020-06-22 10:12:55 +0500

    flake8 + one more expeption

--- attestation/atestation.py
+++ attestation/atestation.py
@@ -15,37 +15,37 @@ class CoffeMaschin:
 
     def __init__(self: Any, dictionary: list) -> None:
         self.dictionary = dictionary
-        self.total_quantity=[]
-
+        self.total_quantity = []
 
     def calculate(self: Any):
         """Calculate ingridients."""
         for item in self.dictionary:
             for key in item:
                 if item.get(key) == "кофе":
-                    if int(item.get('количество'))>int(item.get('порция')):
-                        coffe_dict={item.get(key):item.get('ресурс')}
+                    if int(item.get('количество')) > int(item.get('порция')):
+                        coffe_dict = {item.get(key): item.get('ресурс')}
                         self.total_quantity.append(coffe_dict)
                     else:
                         raise OutOfResourceError("not enought coffe")
-                elif item.get(key) == "вода" and key=='тип':
+                elif item.get(key) == "вода" and key == 'тип':
                     if int(item.get('количество')) > int(item.get('порция')):
                         water_dict = {item.get(key): item.get('ресурс')}
                         self.total_quantity.append(water_dict)
                     else:
-                        raise OutOfResourceError("not enought coffe")
-                elif item.get(key) == "молоко" and key=='тип':
+                        raise OutOfResourceError("not enought water")
+                elif item.get(key) == "молоко" and key == 'тип':
                     if int(item.get('количество')) > int(item.get('порция')):
                         milk_dict = {item.get(key): item.get('ресурс')}
                         self.total_quantity.append(milk_dict)
 
                 elif item.get(key) == "сироп":
                     if int(item.get('количество')) > int(item.get('порция')):
-                        sirop_dict={item.get(key): item.get('ресурс')}
+                        sirop_dict = {item.get(key): item.get('ресурс')}
                         self.total_quantity.append(sirop_dict)
         print(self.total_quantity)
 
-    def menu(self):
+    def menu(self: Any) -> list:
+        """Return all possible combinations."""
         coffe = []
         sirop = []
         milk = []
@@ -61,45 +61,34 @@ class CoffeMaschin:
                 elif key == 'молоко':
                     milk.append(item)
 
-        result = list(itertools.product(coffe, water))+list(itertools.product(coffe, water,milk))\
-                 +list(itertools.product(coffe, water,sirop))+list(itertools.product(coffe, water, sirop, milk))
+        result = list(itertools.product(coffe, water)) + list(itertools.product(coffe, water, milk)) \
+                 + list(itertools.product(coffe, water, sirop)) + list(itertools.product(coffe, water, sirop, milk))
+        if len(result) == 0:
+            raise OutOfResourceError("no resurs")
 
         return result
 
 
 if __name__ == '__main__':
-    a=CoffeMaschin([
-    {
-        "ресурс" : "арабика",
-        "тип" : "кофе",
-        "количество" : 1000,
-        "порция" : 10
-    },
-    {
-        "ресурс": "робуста",
-        "тип": "кофе",
-        "количество": 1000,
-        "порция": 10
-    },
-
-    {
-        "ресурс" : "вода",
-        "тип" : "вода",
-        "количество" : 1000,
-        "порция" : 60
-    },
-    {
-        "ресурс" : "карамель",
-        "тип" : "сироп",
-        "количество" : 200,
-        "порция" : 80
-    },
-    {
-        "ресурс" : "шоколад",
-        "тип" : "сироп",
-        "количество" : 200,
-        "порция" : 80
-    }
-])
+    a = CoffeMaschin([
+        {
+            "ресурс": "арабика",
+            "тип": "кофе",
+            "количество": 5,
+            "порция": 10
+        },
+        {
+            "ресурс": "молоко",
+            "тип": "молоко",
+            "количество": 50,
+            "порция": 60
+        },
+        {
+            "ресурс": "вода",
+            "тип": "вода",
+            "количество": 50,
+            "порция": 60
+        }
+    ])
     a.calculate()
     print(a.menu())
